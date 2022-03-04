223ee08bee0213212bc99eec9dfa17b71b7085f6
Baras Bubeev <73842467+baras-bubeev@users.noreply.github.com>
2022-03-04 14:46:17 +0700

    add main function

--- binSearch.c
+++ binSearch.c
@@ -2,23 +2,28 @@
 
 int ft_binSearch(int *arr, int size, int search){
 // arr - массив для поиска, size - размер массива, search - искомое значение
-	int begin = 0; // начало диапозона
+	int begin = 0;	// начало диапозона
 	int end = size;	// конец диапозона
-	int tmp = 0; // середина
-// в диапозоне левая граница включена, а правая  - нет
+	int tmp = 0;	// середина
+	// диапозон включает левую границу, правую  - нет
 
-	if (!arr) return -1;	// на случай, если вместо массива закинули NULL
+	if (!arr) return -1;				// проверка поданого массива
 	while (begin < end){
-		tmp = begin + (end - begin) / 2;	// именно такая форма вычисления среднего - для избежания переполнения типа
+		tmp = begin + (end - begin) / 2;	// форма вычисления среднего для избежания переполнения типа
 		if (search == arr[tmp]) return tmp;
 		if (search > arr[tmp]) begin = ++tmp;	// ++tmp - для избежания зацикливания
 		else end = tmp;
 	}
-	return -(begin); // если искомого значения нет в массиве, выводим индекс, где он должен быть, со знаком минус
+	return -(begin);	// если искомого значения нет в массиве, выводим индекс, где он должен быть, со знаком минус
 }
 
 int main(){
 	int a[10] = {1, 2, 5, 6, 8, 11, 12};
-	printf("iskomoye znachenie pod indexom %d", ft_binSearch(a, 7, 6));
+	int ret = ft_binSearch(a, 7, 6);
+	
+	if (ret < 0)
+		printf("Value not found! Estimated index: %d", -ret);
+	else
+		printf("Value index: %d", ret);
 	return 0;
 }
